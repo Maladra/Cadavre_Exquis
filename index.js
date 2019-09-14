2c7d0df6e3f85c13878e0ad1c05bc5b0bf02d29d
Maladra <julien.ehles@hotmail.fr>
2019-09-14 18:27:24 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,69 @@
+var express = require('express')
+var app = express()
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+var room_list = [];
+
+app.use(express.static(__dirname + "/public"));
+
+//creation socket
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    
+    // get username for socket
+    socket.on('set_username',function (msg) {
+        socket.username = msg;
+        console.log(socket.username)
+    });
+
+    // on socket disconnect
+    socket.on('disconnect', function () {
+        console.log('user disconnected')
+    });
+
+    // on user join a room
+    // set socket.room_joined
+    socket.on('join_room', function (msg) {
+        if(room_exist(msg)){
+            socket.join(msg)
+            console.log("la salle existe deja")
+        }
+        else{
+            socket.join(msg)
+            console.log("la salle n'existe pas")
+        }
+        socket.room_joined = msg
+        console.log(socket.room_joined)
+
+
+        socket.on('chat message', function (msg) {
+            console.log('message: ' + msg);
+            console.log(socket.room_joined)
+            io.in(socket.room_joined).emit('chat message', socket.username +" : " + msg);
+        });
+
+
+        socket.on('disconnect', function(){
+            console.log("room leaved")
+        })
+    });
+
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+
+
+function room_exist(msg) {
+    var existe = false
+    for (let key in io.sockets.adapter.rooms) // permet d'avoir la liste des rooms
+    {
+        if(msg == key){
+            existe = true;
+            break;
+        }
+    }
+    return existe;
+};
---
+++ public/index_front.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    $('#select_username').fadeIn("slow", function () {
+    });
+});
+
+$(function () {
+var socket = io(); // initialise socket
+var username; // username for the socket
+
+// function for send username to server 
+const send_username = function () {
+    $('#select_username').fadeOut("fast", function (){
+        $('#join_select_room').fadeIn("fast")
+    });
+    username = $('#username').val();
+    socket.emit('set_username', username)
+}
+
+// function for join or create room 
+const join_room = function () {
+    $('#join_select_room').fadeOut("fast", function (){
+        room_name = $('#choose_room').val();
+        alert (room_name)
+        socket.emit('join_room', room_name)
+        
+        $('#game_room').fadeIn("fast")
+    });
+    
+}
+
+// event send username on server display the room selector
+$('#validate_username').click(function () {
+    send_username()
+});
+
+// event send create/join socket on server and display game room
+$('#validate_room').click(function () {
+    join_room()
+});
+
+$('form').submit(function(e){
+    e.preventDefault(); // prevents page reloading
+    
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+});
+socket.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+  });
+});
